a580c36264aa0cd565de47bff6da1d145aaf24a1
Mateus Cimini Araujo <mateusmateus122333@gmail.com>
2024-07-12 21:11:07 -0300

    add new repository

---
+++ script.js
@@ -0,0 +1,12 @@
+ var altura  = 2.00
+
+if (altura >= 1.80) {
+    console.log(`Voce e alto vc tem ${altura}cm de altura`)
+} else if (altura <= 1.79 && altura >= 1.70 ) {
+    console.log(`Voce tem altura normal vc tem ${altura}cm de altura`)
+} else {
+    console.log(`Voce e baixo tem somente ${altura}cm de altura`)
+}
+
+
+// aula de js fazendo condiçoes composta fazendo aparecer no Node.Js usando a extençao Node exec
